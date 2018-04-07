bf79ee2419b7c4af588144d81e51fbfc408f66dc
SUK HWA HAM <hamsukhwa@gmail.com>
2018-04-07 00:27:26 -0400

    modify the exercise

--- exercise.py
+++ exercise.py
@@ -1,13 +1,12 @@
 import cv2
+import glob
 
-kangaroos = cv2.imread("kangaroos-rain-australia_71370_990x742.jpg", 0)
-lighthouse = cv2.imread("Lighthouse.jpg", 0)
-moon = cv2.imread("Moon sinking, sun rising.jpg", 0)
+images = glob.glob("*.jpg")
 
-kangaroo_resize = cv2.resize(kangaroos,(100,100))
-lighthouse_resize = cv2.resize(lighthouse, (100,100))
-moon_resize = cv2.resize(moon, (100,100))
-
-cv2.imwrite("kangaroo_resized.jpg", kangaroo_resize)
-cv2.imwrite("lighthouse_resized.jpg", lighthouse_resize)
-cv2.imwrite("moon_resize.jpg", moon_resize)
+for image in images:
+    img = cv2.imread(image,0)
+    re = cv2.resize(img, (100, 100))
+    cv2.imshow("Hey", re)
+    cv2.waitKey(500)
+    cv2.destroyAllWindows()
+    cv2.imwrite("resized_" + image,re)
